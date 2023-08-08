b57beb3a608b1e81f8452df0913f1de891d892e2
ali-jameel1 <alijameel4545@gmail.com>
2023-08-08 11:50:34 -0400

    port 80

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.post('/create-checkout-session', async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
